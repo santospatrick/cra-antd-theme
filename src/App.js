2f6c8126ec1ce4e2491ed1dae818edb4be80a9f4
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-17 22:35:27 -0300

    feat: add styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Button, Space, Switch } from 'antd';
 import './App.less';
+import styled from 'styled-components';
+
+const StyledSquare = styled.div`
+  height: 50px;
+  width: 50px;
+  background-color: red;
+`
 
 const App = () => (
   <div className="App">
     <Space size={20} direction="vertical">
+      <StyledSquare />
       <Button type="primary">Button</Button>
       <Switch defaultChecked />
     </Space>
